826ac2b0f3cc7b6e41b1de02b6128e08032f34e3
ごま <gomayumax@users.noreply.github.com>
2017-08-27 14:41:49 +0900

    install electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'neta_image_finder'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
